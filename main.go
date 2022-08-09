b84e62bf3d08804b780191e32fa9465bc77ec2a5
patrickpolii <poliipatrick07@gmail.com>
2022-08-09 11:02:04 +0700

    add middlewares

--- main.go
+++ main.go
@@ -3,24 +3,27 @@ package main
 import (
 	"order-api/pkg/config"
 	"order-api/pkg/controllers"
+	"order-api/pkg/helper"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	//load env
 	godotenv.Load()
+
 	routes := gin.Default()
 
 	// Connect to database
 	config.ConnectDatabase()
 
 	// Routes Orders
-	routes.GET("/orders", controllers.FindOrders)
-	routes.GET("/orders/:id", controllers.FindOrder)
-	routes.POST("/orders", controllers.CreateOrder)
-	routes.PATCH("/orders/:id", controllers.UpdateOrder)
-	routes.DELETE("/orders/:id", controllers.DeleteOrder)
+	routes.GET("/orders", helper.JwtAuthMiddleware(), controllers.FindOrders)
+	routes.GET("/orders/:id", helper.JwtAuthMiddleware(), controllers.FindOrder)
+	routes.POST("/orders", helper.JwtAuthMiddleware(), controllers.CreateOrder)
+	routes.PATCH("/orders/:id", helper.JwtAuthMiddleware(), controllers.UpdateOrder)
+	routes.DELETE("/orders/:id", helper.JwtAuthMiddleware(), controllers.DeleteOrder)
 
 	//Routes Auth
 	routes.POST("/register", controllers.Register)
---
+++ pkg/helper/middlewares.go
@@ -0,0 +1,20 @@
+package helper
+
+import (
+	"net/http"
+	"order-api/pkg/utils"
+
+	"github.com/gin-gonic/gin"
+)
+
+func JwtAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		err := utils.TokenValid(c)
+		if err != nil {
+			c.String(http.StatusUnauthorized, "Unauthorized")
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
